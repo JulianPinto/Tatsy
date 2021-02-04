4e6a31fc7d5e5f741ed3ce5149ebf292ff10a6b0
Julian Pinto <julianpinto@shaw.ca>
2021-02-03 22:29:52 -0700

    starting controller for mlink

---
+++ desktopApp/Controls/mlink.cpp
@@ -0,0 +1,7 @@
+// standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// define section
+#define SOMETHING 0xff
---
+++ desktopApp/Controls/setupAll.cpp
@@ -0,0 +1,16 @@
+// standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// define section
+#define SOMETHING 0xff
+
+// global vars
+int global;
+
+// no arguments
+int main(int argc, char * argv[])
+{
+    return 0;
+}
