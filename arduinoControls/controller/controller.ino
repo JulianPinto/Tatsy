7427029ce2e7d71cf7e3405d33a5d35c4fc612fc
GMKBrown <gesiyek1@gmail.com>
2021-02-21 20:15:11 -0700

    Further changes to arduino MLink communication
    hopefully it works

--- arduinoControls/controller/controller.ino
+++ arduinoControls/controller/controller.ino
@@ -11,18 +11,80 @@
 #define DATA      0x14
 #define ERROR     0x02
 
+// Reset
+void resetIndication (uint8_t flags,
+                      uint8_t hwRev,
+                      uint16_t hwFault,
+                      uint16_t fwMajorRev,
+                      uint16_t fwMinorRev,
+                      uint16_t fwFlags,
+                      uint8_t region);
 
+// Start
+void startIndication (uint8_t outcome,
+                      uint32_t serial);
+void activateIndication ();
+
+// Stop
+void stopIndication (uint8_t reason);
+
+// Data
+void transmitStatusIndication (uint8_t handle,
+                               uint8_t outcome,
+                               int8_t rssi);
+
+// Error Checking
+void errorIndication (uint8_t ackId,
+                      uint8_t reason);
+
+// Acknowledgment
+void acknowledgeMessage(uint8_t messageId);
+
+// Used to check when there is a callback
+bool hasReset = false;
+bool hasStarted = false;
+bool hasActivated = false;
+bool hasSentData = false;
+
+AltSoftSerial mlink(resetIndication,
+                    startIndication,
+                    activateIndication,
+                    stopIndication,
+                    transmitStatusIndication,
+                    errorIndication,
+                    acknowledgeMessage);
+
+/*----------------------------------------- Data Transfer Setup -------------------------*/
+// Timing Parameters
+long intervalLed = 2000;  // Blink LED every 2 seconds
+long intervalData = 1000; // Send voltage reading from a potentiometer every second
+unsigned long currentMillis;
+unsigned long previousMillisLed = 0;
+unsigned long previousMillisData = 0;
+
+//data sending
+#define ini_1 0xA0
+#define ini_2 0xA1
+#define poll_length 0x0001
+#define poll_stat 0x11
+#define end_1 0xB0
+#define end_2 0xB1
+
+boolean newData = false;
+boolean recvInProgress = false; //whether or not data is being recieved
+
+/*----------------------------------------- LED Setup -------------------------*/
+const uint8_t ledPin = 13;
+uint8_t ledState = LOW;
 
 /*----------------------------------------- Servo Setup -------------------------*/
 //pulse widths for esc in milliseconds
 const int min_pw = 1000;
 const int max_pw = 2000;
 int ESC_val;
-
-
-//data sending
-boolean newData = false;
-boolean recvInProgress = false; //whether or not data is being recieved
+//Joystick Value
+uint16_t joyData;
+boolean dir;
 
 //motor setup
 #define ENA 10
@@ -37,6 +99,13 @@ void setup() {
   Serial.begin(57600);
   Serial.flush();
 
+  //connect and reeset MLink Endpoint
+  mlink.begin (57600);
+  mlink.resetRequest(); // resetting MLink
+
+  //Led Pin
+  pinMode(ledPin, OUTPUT);
+
   //initiallizing motor pins
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
@@ -47,5 +116,20 @@ void setup() {
 }
 
 void loop() {
-  recvWithStartEndMarkers();
+  currentMillis = millis();
+  BlinkLed();
+  sendStart();
+  sendData();
+  recv_position();
+  if (joyData != 0) {
+    if (dir) {
+      forward(conv_speed(joyData));
+    }
+    else {
+      backward(conv_speed(joyData));
+    }
+  }
+  else {
+    stay();
+  }
 }
--- arduinoControls/controller/functions.ino
+++ arduinoControls/controller/functions.ino
@@ -1,42 +1,81 @@
 /*----------------------------------------- MLink functions -------------------------*/
-// Reset
 void resetIndication (uint8_t flags,
                       uint8_t hwRev,
                       uint16_t hwFault,
                       uint16_t fwMajorRev,
                       uint16_t fwMinorRev,
                       uint16_t fwFlags,
-                      uint8_t region);
+                      uint8_t region)
+{
+  hasReset = true;
+}
 
-// Start
 void startIndication (uint8_t outcome,
-                      uint32_t serial);
-void activateIndication ();
+                      uint32_t serial)
+{
+  hasStarted = true;
+}
 
-// Stop
-void stopIndication (uint8_t reason);
+void activateIndication ()
+{
+  hasActivated = true;
+}
 
-// Data
+void stopIndication (uint8_t reason)
+{
+
+}
+
 void transmitStatusIndication (uint8_t handle,
                                uint8_t outcome,
-                               int8_t rssi);
+                               int8_t rssi)
+{
+  hasSentData = true;
+}
 
-// Error Checking
 void errorIndication (uint8_t ackId,
-                      uint8_t reason);
+                      uint8_t reason)
+{
 
-// Acknowledgment
-void acknowledgeMessage(uint8_t messageId);
+}
 
+void acknowledgeMessage (uint8_t messageId)
+{
+  mlink.acknowledge (messageId);
+}
+
+// sending start request to MLink
+void sendStart()
+{
+  if (hasReset && !hasStarted)
+  {
+    // Please review the user manual for information
+    // on startRequest parameters
+    mlink.startRequest(0x00, 0x00, 0x00, 0xFF);
+    hasStarted = true;
+  }
+}
+
+// sending voltage values from a potentiometer
+void sendData()
+{
+  if (currentMillis - previousMillisData >= intervalData)
+  {
+    previousMillisData = currentMillis;
+    if (hasActivated)
+    {
+      uint16_t analogData = analogRead(A1);
+      mlink.dataRequest(analogData, 0x02, 0);
+    }
+  }
+}
 
 /*----------------------------------------- Data Receive -------------------------*/
 //receiving joystick data
-void recvWithStartEndMarkers() {
+void recv_position() {
   static byte ndx = 0; //current index
-  char startMarker = '<'; //initiallization marker
-  char endMarker = '>'; //end marker
-  char rc; //received character
-
+  static uint8_t rc; //received character
+  static uint8_t received_data[11];
   // if ther'es a serial connection and a dataset isn't currently being sent
   while (Serial.available() > 0 && newData == false) {
     rc = Serial.read();
@@ -44,28 +83,31 @@ void recvWithStartEndMarkers() {
     //if data is being received
     if (recvInProgress == true) {
       //if the character received isn't the end marker
-      if (rc != endMarker) {
-        receivedChars[ndx] = rc; // add character to character array
+      if (rc != end_2 && received_data[ndx] != end_1) {
+        received_data[ndx] = rc; // add character to character array
         ndx++;
-        //subtracts number of available characters in array
-        if (ndx >= numChars) {
-          ndx = numChars - 1;
-        }
       }
       //terminates and resets string
       else {
-        receivedChars[ndx] = '\0'; // terminate the string
         recvInProgress = false;
         ndx = 0;
         newData = true;
         //converts string to joystick value
         //joystickVal = atoi(receivedChars);
-        
+        joyData = received_data[5];
       }
     }
 
-    else if (rc == startMarker) {
-      recvInProgress = true;
+    else if (rc == ini_1) {
+      //checks to see if the first part of the message start was sent
+      received_data[ndx] = rc;
+      ndx++;
+      if (rc == ini_2) {
+        //checks to see if the second part of the message start was sent
+        recvInProgress = true;
+        received_data[ndx] = rc;
+        ndx++;
+      }
     }
   }
 }
@@ -73,18 +115,64 @@ void recvWithStartEndMarkers() {
 
 
 /*----------------------------------------- Motor Functions -------------------------*/
+//speed conversion
+int conv_speed(int speed_val) {
+  static int pwmOutput;
+  speed_val = speed_val - 100;
+
+  if (speed_val >= 0) {
+    dir = true;
+    pwmOutput = map(speed_val, 0, 100, 0 , 255);
+    return pwmOutput;
+  }
+  else {
+    dir = false;
+    speed_val = speed_val * -1;
+    pwmOutput = map(speed_val, 0, 100, 0 , 255);
+    return pwmOutput;
+  }
+}
+
+
 //move forwards
-void forward() {
-  analogWrite(ENA, 128);
-  analogWrite(ENB, 128);
+void forward(int vel) {
+  analogWrite(ENA, vel);
+  analogWrite(ENB, vel);
   digitalWrite(IN1, HIGH);
   digitalWrite(IN3, HIGH);
 }
 
 //move backwards
-void backward() {
-  analogWrite(ENA, 128);
-  analogWrite(ENB, 128);
+void backward(int vel) {
+  analogWrite(ENA, vel);
+  analogWrite(ENB, vel);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN4, HIGH);
+}
+
+//stop
+void stay() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+/*----------------------------------------- LED Functions -------------------------*/
+// Blinking the LED
+void BlinkLed()
+{
+  if (currentMillis - previousMillisLed >= intervalLed)
+  {
+    previousMillisLed = currentMillis;
+    if (ledState == LOW)
+    {
+      ledState = HIGH;
+    }
+    else
+    {
+      ledState = LOW;
+    }
+    digitalWrite (ledPin, ledState);
+  }
 }
